8c3d0d3d24b35e5de8ee737e05d3ab7004ed8bb7
Simon Tucker <brotherlogic@gmail.com>
2022-01-19 14:46:05 -0800

    Trip the name

--- display.go
+++ display.go
@@ -65,7 +65,7 @@ func (s *Server) handler(ctx context.Context) {
 				<img class="art_image" src="https://img.discogs.com/VsImbPqwzP7fNEM_Ws_y7Lkbh7Q=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10604586-1500801461-4954.jpeg.jpg" width="500" height="500">
 				<div class="text">
 					<div class="artist">Bernard Estardy</div>
-					<div class="album">Piano & Orgues</div>
+					<div class="album">Piano Et Orgues</div>
 				</div>		
 			</section>		
 		</div>
