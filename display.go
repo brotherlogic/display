6b3b61eb72f3908828de641a0c148acdd177f164
Simon Tucker <brotherlogic@gmail.com>
2022-01-20 09:45:08 -0800

    Bounce the display

--- display.go
+++ display.go
@@ -64,7 +64,7 @@ func (s *Server) handler(ctx context.Context) {
 			<section id="main">
 				<img class="art_image" src="https://img.discogs.com/VsImbPqwzP7fNEM_Ws_y7Lkbh7Q=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10604586-1500801461-4954.jpeg.jpg" width="500" height="500">
 				<div class="text">
-					<div class="artist">Bernard Estardy</div>
+					<div class="artist">Atrio</div>
 					<div class="album">Piano Et Orgues</div>
 				</div>		
 			</section>		
