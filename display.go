2c993272dcffafb0689358d8ef03cdc5eadb69f2
Simon Tucker <brotherlogic@gmail.com>
2022-01-21 07:49:32 -0800

    Also change the image

--- display.go
+++ display.go
@@ -60,6 +60,7 @@ func (s *Server) GetState() []*pbg.State {
 type temp struct {
 	Title  string
 	Artist string
+	Image  string
 }
 
 func (s *Server) buildPage(ctx context.Context) {
@@ -69,13 +70,13 @@ func (s *Server) buildPage(ctx context.Context) {
 
 		r, err := client.GetRecord(ctx, &pbrg.GetRecordRequest{Refresh: true})
 		if err == nil {
-			s.handler(ctx, r.GetRecord().GetRelease().GetTitle(), r.GetRecord().GetRelease().GetArtists()[0].GetName())
+			s.handler(ctx, r.GetRecord().GetRelease().GetTitle(), r.GetRecord().GetRelease().GetArtists()[0].GetName(), r.GetRecord().GetRelease().GetImages()[0].GetUri())
 		}
 
 	}
 }
 
-func (s *Server) handler(ctx context.Context, title, artist string) {
+func (s *Server) handler(ctx context.Context, title, artist, image string) {
 	t := template.New("page")
 	t, err := t.Parse(`<html>
 	<meta http-equiv="refresh" content="60">
@@ -83,7 +84,7 @@ func (s *Server) handler(ctx context.Context, title, artist string) {
 		<div id="container">	
 			<div class="artwork"></div>
 			<section id="main">
-				<img class="art_image" src="https://img.discogs.com/VsImbPqwzP7fNEM_Ws_y7Lkbh7Q=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10604586-1500801461-4954.jpeg.jpg" width="500" height="500">
+				<img class="art_image" src="{{.Image}}" width="500" height="500">
 				<div class="text">
 					<div class="artist">{{.Artist}}</div>
 					<div class="album">{{.Title}}</div>
@@ -101,7 +102,8 @@ func (s *Server) handler(ctx context.Context, title, artist string) {
 
 	t.Execute(f, &temp{
 		Title:  title,
-		Artist: artist})
+		Artist: artist,
+		Image:  image})
 
 	conn, err := s.FDialServer(ctx, "filecopier")
 	fc := fcpb.NewFileCopierServiceClient(conn)
