6efca9b3bf203832ae26238599e95e9de9710647
Simon Tucker <brotherlogic@gmail.com>
2022-08-01 09:04:07 -0700

    Revert to non-queued copies

--- display.go
+++ display.go
@@ -225,7 +225,7 @@ func (s *Server) handler(ctx context.Context, title, artist, image, extra string
 	defer conn.Close()
 
 	fc := fcpb.NewFileCopierServiceClient(conn)
-	_, err = fc.QueueCopy(ctx, &fcpb.CopyRequest{
+	_, err = fc.Copy(ctx, &fcpb.CopyRequest{
 		InputServer:  s.Registry.Identifier,
 		InputFile:    "/media/scratch/display/display.html",
 		OutputServer: "rdisplay",
@@ -236,7 +236,7 @@ func (s *Server) handler(ctx context.Context, title, artist, image, extra string
 		activity.With(prometheus.Labels{"message": fmt.Sprintf("COPY_HTML: %v", err)}).Inc()
 		return err
 	}
-	_, err = fc.QueueCopy(ctx, &fcpb.CopyRequest{
+	_, err = fc.Copy(ctx, &fcpb.CopyRequest{
 		InputServer:  s.Registry.Identifier,
 		InputFile:    "/media/scratch/display/style.css",
 		OutputServer: "rdisplay",
@@ -247,7 +247,7 @@ func (s *Server) handler(ctx context.Context, title, artist, image, extra string
 		activity.With(prometheus.Labels{"message": fmt.Sprintf("COPY_CSS: %v", err)}).Inc()
 		return err
 	}
-	_, err = fc.QueueCopy(ctx, &fcpb.CopyRequest{
+	_, err = fc.Copy(ctx, &fcpb.CopyRequest{
 		InputServer:  s.Registry.Identifier,
 		InputFile:    "/media/scratch/display/normalize.css",
 		OutputServer: "rdisplay",
@@ -258,7 +258,7 @@ func (s *Server) handler(ctx context.Context, title, artist, image, extra string
 		activity.With(prometheus.Labels{"message": fmt.Sprintf("COPY_NORM: %v", err)}).Inc()
 		return err
 	}
-	_, err = fc.QueueCopy(ctx, &fcpb.CopyRequest{
+	_, err = fc.Copy(ctx, &fcpb.CopyRequest{
 		InputServer:  s.Registry.Identifier,
 		InputFile:    "/media/scratch/display/image.jpeg",
 		OutputServer: "rdisplay",
