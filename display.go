ef6a3f19e972732200e5fd29d2267f093de76ee7
Simon Tucker <brotherlogic@gmail.com>
2022-09-02 12:53:59 -0700

    Limit image size

--- display.go
+++ display.go
@@ -179,7 +179,7 @@ func (s *Server) handler(ctx context.Context, title, artist, image, extra string
 		<div id="container">	
 			<div class="artwork"></div>
 			<section id="main">
-				<img class="art_image" src="image.jpeg" width="400" height="400">
+				<img class="art_image" src="image.jpeg" width="10" height="10">
 				<div class="text">
 					<div class="artist">{{.Artist}}</div>
 					<div class="album">{{.Title}}</div>
